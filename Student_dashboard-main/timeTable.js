a6131563f5f606176d60cebb57f39d278f046ddd
rahul012000 <rahulchaurasia041@gmail.com>
2024-11-27 13:47:29 +0530

    Add files via upload

---
+++ Student_dashboard-main/timeTable.js
@@ -0,0 +1,256 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '209',
+        subject: 'DS',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '209',
+        subject: 'ECE',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-01 PM',
+        roomNumber: '110',
+        subject: 'WDW Lab',
+        type: 'Lab'
+    
+    },
+    {
+        time: '01-02 PM',
+        roomNumber: '209',
+        subject: 'Lunch',
+        type: 'Lunch'
+    },
+    {
+        time: '02-03 PM',
+        roomNumber: '209',
+        subject: 'COA',
+        type: 'Lecture'
+    },
+    {
+        time: '03-04 PM',
+        roomNumber: '209',
+        subject: 'TC',
+        type: 'Lecture'
+    },
+    {
+        time: '04-05 PM',
+        roomNumber: '209',
+        subject: 'CS',
+        type: 'Lecture'
+    },
+]
+const Tuesday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '209',
+        subject: 'DSTL',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '209',
+        subject: 'COA',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 PM',
+        roomNumber: '209',
+        subject: 'CS',
+        type: 'Lecture'
+    
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '209',
+        subject: 'DS',
+        type: 'Lecture'
+    
+    },
+    {
+        time: '01-02 PM',
+        roomNumber: '209',
+        subject: 'Lunch',
+        type: 'Lunch'
+    },
+    {
+        time: '02-03 PM',
+        roomNumber: '209',
+        subject: 'ECE',
+        type: 'Lecture'
+    },
+    {
+        time: '03-05 PM',
+        roomNumber: '314',
+        subject: 'Mini Project',
+        type: 'Lab'
+    },
+    
+]
+
+const Wednesday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '209',
+        subject: 'DSTL',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '209',
+        subject: 'TC',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-01 PM',
+        roomNumber: '110',
+        subject: 'COA Lab',
+        type: 'Lab'
+    
+    },
+    {
+        time: '01-02 PM',
+        roomNumber: '209',
+        subject: 'Lunch',
+        type: 'Lunch'
+    },
+    {
+        time: '02-03 PM',
+        roomNumber: '209',
+        subject: 'COA',
+        type: 'Lecture'
+    },
+    {
+        time: '03-04 PM',
+        roomNumber: '209',
+        subject: 'ECE',
+        type: 'Lecture'
+    },
+    {
+        time: '04-05 PM',
+        roomNumber: '209',
+        subject: 'DS',
+        type: 'Lecture'
+    },
+]
+
+const Thursday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '209',
+        subject: 'DS',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '209',
+        subject: 'DSTL',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 PM',
+        roomNumber: '209',
+        subject: 'COA',
+        type: 'Lecture'
+    
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '209',
+        subject: 'ECE',
+        type: 'Lecture'
+    
+    },
+    {
+        time: '01-02 PM',
+        roomNumber: '209',
+        subject: 'Lunch',
+        type: 'Lunch'
+    },
+    {
+        time: '02-03 PM',
+        roomNumber: '209',
+        subject: 'CS',
+        type: 'Lecture'
+    },
+    {
+        time: '03-04 PM',
+        roomNumber: '209',
+        subject: 'DSTL',
+        type: 'Lecture'
+    },
+    {
+        time: '04-05 PM',
+        roomNumber: '',
+        subject: '',
+        type: 'Library'
+    },
+]
+
+const Friday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '209',
+        subject: 'DS',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '209',
+        subject: 'COA',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 PM',
+        roomNumber: '209',
+        subject: 'DSTL',
+        type: 'Lecture'
+    
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '209',
+        subject: 'ECE',
+        type: 'Lecture'
+    
+    },
+    {
+        time: '01-02 PM',
+        roomNumber: '209',
+        subject: 'Lunch',
+        type: 'Lunch'
+    },
+    {
+        time: '02-03 PM',
+        roomNumber: '209',
+        subject: 'TC',
+        type: 'Lecture'
+    },
+    {
+        time: '03-05 PM',
+        roomNumber: '312',
+        subject: 'DS',
+        type: 'Lab'
+    },
+    
+
+]
+const Saturday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
